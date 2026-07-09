cecdc4f3ecf28789ba5df43a78c5484b7d99686b
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-07-09 13:08:48 -0300

    feat - criação do upload de arquivos

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res) => {
-    res.json({
-        mensagem: "Rota de upload funcionando!"
+const upload = require("../middlewares/uploadMiddleware");
+
+// Rota para upload de arquivos
+router.post("/", upload.single("arquivo"), (req, res) => {
+
+    if (!req.file) {
+        return res.status(400).json({ 
+            error: "Nenhum arquivo enviado!" 
+        });
+    }
+
+    res.status(200).json({
+        message: "Arquivo enviado com sucesso!",
+        arquivo: req.file.filename
     });
 });
 
