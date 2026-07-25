fe0a009e7da9b493901967d1ffbad0126a819ca0
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-07-25 12:27:36 -0300

    feat: implementação do KPIGrid

--- frontend/src/components/layout/MainContent.jsx
+++ frontend/src/components/layout/MainContent.jsx
@@ -1,22 +1,50 @@
+import { useEffect, useState  } from "react";
+import axios from "axios";
 import KPIGrid from "../dashboard/KPIGrid";
 
 function MainContent() {
+
+    const [indicadores, setIndicadores] = useState(null);
+
+    useEffect(() => {
+
+        async function carregarIndicadores() {
+
+            try {
+
+                const response = await axios.get("http://localhost:3000/api/indicadores");
+
+                setIndicadores(response.data);
+
+            } catch (error) {
+
+                console.error("Erro ao buscar indicadores:", error);
+
+            }
+
+        }
+
+        carregarIndicadores();
+
+    }, []);
+
     return (
         <main className="flex-1 bg-slate-100 p-6">
 
-            <div className="bg-white rounded-x1 shadow p-6 h-full">
+            <div className="bg-white rounded-xl shadow p-6 h-full">
 
-                <h2 className="text-3x1 font-bold">
-                    Dashboard ATENAS-IA
+                <h2 className="text-3xl font-bold">
+                    Dashboard Atenas IA
                 </h2>
 
-                <P className="text-slade-500 mt-2 mb-8">
+                <p className="text-slate-500 mt-2 mb-8">
                     Indicadores da análise processada.
-                </P>
+                </p>
 
-                <KPIGrid />
+                <KPIGrid dados={indicadores} />
 
             </div>
+
         </main>
     );
 }
